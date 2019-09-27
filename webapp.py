d3a11b9a2f272efd1b6a1cc1e57a93babf1ecf48
Ryan Ly <rly@lbl.gov>
2019-09-26 18:54:35 -0700

    Rename main.py -> webapp.py, add ping pong hook

--- webapp.py
+++ webapp.py
@@ -14,6 +14,19 @@ class MainHandler(RequestHandler):
         self.write('Hello, world')
 
 
+class CommandHookHandler(RequestHandler):
+    def post(self):
+        headers = self.request.headers
+        event = headers.get('X-GitHub-Event', None)
+
+        if event == 'ping':
+            self.write('pong')
+        else:
+            print('Unhandled event "{}".'.format(event))
+            self.set_status(404)
+            self.write_error(404)
+
+
 async def shutdown():
     logging.info('Stopping server')
     http_server.stop()
@@ -26,9 +39,10 @@ def exit_handler(sig, frame):
     IOLoop.instance().add_callback_from_signal(shutdown)
 
 
-def start_server():
+def create_webapp():
     urls = [
         (r'/', MainHandler),
+        (r'/nwb-extensions-command/hook', CommandHookHandler)
     ]
     return Application(urls)
 
@@ -43,9 +57,21 @@ if __name__ == '__main__':
     signal.signal(signal.SIGTERM, exit_handler)
     signal.signal(signal.SIGINT, exit_handler)
 
-    app = start_server()
-    http_server = HTTPServer(app)
+    app = create_webapp()
+    http_server = HTTPServer(app, xheaders=True)
     port = int(os.getenv('PORT', 8888))
-    http_server.listen(port)
+
+    # https://devcenter.heroku.com/articles/optimizing-dyno-usage#python
+    n_processes = int(os.environ.get("WEB_CONCURRENCY", 1))
+
+    if n_processes != 1:
+        logging.info('Multiple processes available. Binding to port %d' % port)
+        # http://www.tornadoweb.org/en/stable/guide/running.html#processes-and-ports
+        http_server.bind(port)
+        http_server.start(n_processes)
+    else:
+        logging.info('Listening to port %d' % port)
+        http_server.listen(port)
+
     logging.info('Starting server')
     IOLoop.current().start()
