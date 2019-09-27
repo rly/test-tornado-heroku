f3a7c31dfa667daaace606b3e24eb668f13e69b4
Ryan Ly <rly@lbl.gov>
2019-09-26 18:36:45 -0700

    Init hello world

---
+++ main.py
@@ -0,0 +1,50 @@
+import logging
+import signal
+import sys
+from tornado.ioloop import IOLoop
+from tornado.web import Application, RequestHandler
+from tornado.httpserver import HTTPServer
+from tornado import gen
+
+from tornado.log import enable_pretty_logging
+
+
+class MainHandler(RequestHandler):
+    def get(self):
+        self.write('Hello, world')
+
+
+async def shutdown():
+    logging.info('Stopping server')
+    http_server.stop()
+    await gen.sleep(1)
+    IOLoop.current().stop()
+
+
+def exit_handler(sig, frame):
+    logging.warning('Caught signal: %s', sig)
+    IOLoop.instance().add_callback_from_signal(shutdown)
+
+
+def start_server():
+    urls = [
+        (r'/', MainHandler),
+    ]
+    return Application(urls)
+
+
+handler = logging.FileHandler('tornado.log')
+app_log = logging.getLogger('tornado.application')
+app_log.setLevel(logging.INFO)
+enable_pretty_logging()
+app_log.addHandler(handler)
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGTERM, exit_handler)
+    signal.signal(signal.SIGINT, exit_handler)
+
+    app = start_server()
+    http_server = HTTPServer(app)
+    http_server.listen(8888)
+    logging.info('Starting server')
+    IOLoop.current().start()
